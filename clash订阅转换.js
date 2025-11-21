f2cceb0b86af39e2a3dd35fd9fc2be1e85318c4f
mrsing <786694928@qq.com>
2025-11-21 10:27:49 +0800

    Update clash订阅转换.js

--- clash订阅转换.js
+++ clash订阅转换.js
@@ -88,8 +88,7 @@ function main(config) {
       interval: 300,
     },
     {
-      icon: "https://testingcf.jsdelivr.net/gh/Orz-3/mini@master/Color/Global.png",
-      "include-all": true,
+      icon: "https://testingcf.jsdelivr.net/gh/Orz-3/mini@master/Color/Global.png",      
       "exclude-filter": "(?i)GB|Traffic|Expire|Premium|频道|订阅|ISP|流量|到期|重置",
       proxies: ["PROXY"],
       name: "GLOBAL",
